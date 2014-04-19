8eb8ff5575e1b49d0ded228e34a606e271fd63d5
LG <lt2010cuhk@gmail.com>
2014-04-19 21:48:02 +0800

    finish setting view

--- New2048/New2048/Global.h
+++ New2048/New2048/Global.h
@@ -9,6 +9,9 @@
 #ifndef __New2048__Global__
 #define __New2048__Global__
 
-#include <iostream>
+//#include <iostream>
+
+#define IS_OS_7_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+#define IS_IPHONE5 ([UIScreen mainScreen].bounds.size.height > 560.0)
 
 #endif /* defined(__New2048__Global__) */
